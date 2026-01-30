95bca7181d073fc2a9f189eb9f368b81c7525ebb
Diego Vilhalva <diegorocha450@yahoo.com>
2026-01-30 11:16:18 -0300

    Project setup

---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+
+
+import Home from "@/pages/Home"
+import { createBrowserRouter } from "react-router"
+
+
+
+export const router = createBrowserRouter([
+    {
+        path:"/",
+        Component:Home
+    }
+])
