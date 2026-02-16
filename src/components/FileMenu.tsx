fe0c493dd9db326648a4ff2ff89cf58cf702f11c
Diego Vilhalva <diegorocha450@yahoo.com>
2026-02-16 10:55:35 -0300

    create download file functionality

---
+++ src/components/FileMenu.tsx
@@ -0,0 +1,66 @@
+import type { File } from "@/types/all-types"
+import { useState } from "react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { CopyIcon, DownloadIcon, EditIcon, EllipsisVerticalIcon, FolderOpenIcon, InfoIcon, ShareIcon, Trash2Icon } from "lucide-react"
+import { downloadFile } from "@/lib/utils"
+
+
+const FileMenu = ({ file }: { file: File }) => {
+    const [renameOpen, setRenameOpen] = useState(false)
+    const [deleteOpen, setDeleteOpen] = useState(false)
+    const [infoOpen, setInfoOpen] = useState(false)
+    const [detailsOpen, setDetailsOpen] = useState(false)
+    return (
+        <>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <EllipsisVerticalIcon />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-[180px]">
+                    <DropdownMenuItem className="cursor-pointer" onClick={() => setDetailsOpen(true)}>
+                        <FolderOpenIcon size={18} className="text-gray-400" />
+                        Open File
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="cursor-pointer" onClick={() => downloadFile(file.url,file.name)}>
+                        <DownloadIcon />
+                        Download
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="cursor-pointer" onClick={() => setRenameOpen(true)}>
+                        <EditIcon />
+                        Rename
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger>
+                            <ShareIcon />
+                            Share
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <DropdownMenuItem>
+                                    <CopyIcon />
+                                    Copy Link
+                                </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+
+                    <DropdownMenuItem className="cursor-pointer" onClick={() => setInfoOpen(true)}>
+                        <InfoIcon />
+                        File Info
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                     <DropdownMenuItem variant="destructive" className="cursor-pointer" onClick={() => setDeleteOpen(true)}>
+                        <Trash2Icon />
+                        Delete File
+                    </DropdownMenuItem>
+
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    )
+}
+
+export default FileMenu
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,70 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+import { formatRelative, isSameYear, format } from "date-fns"
+import { ur } from "zod/v4/locales"
+
+export const toTileCase = (str: string) => {
+return str[0].toUpperCase() + str.slice(1)
+}
+
+
+export function formatCustomDate(date: string | number | Date) {
+  const today = new Date()
+
+  const relativeDay = toTileCase(formatRelative(date, today).split(" at ")[0])
+
+  const relativeDays = [
+      "Today",
+      "Tomorrow",
+      "Yesterday",
+      "Saturday",
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday"
+  ]
+
+  if (relativeDays.includes(relativeDay)) {
+    return relativeDay
+  }
+
+  if (isSameYear(date,today)) {
+    return format(date, "dd MMM")
+  }else{
+    return format(date,"dd MMM yyyy")
+  }
+}
+
+
+export async function downloadFile(url:string,fileName:string){
+  try {
+    const response = await fetch(url,{method:"GET"})
+
+    if (!response.ok) {
+      throw new Error(`Failed to download: ${response.status}`)
+    }
+
+    const blob = await response.blob()
+    const downloadUrl = window.URL.createObjectURL(blob)
+
+    const a = document.createElement("a")
+
+    a.href = downloadUrl
+    a.download = fileName || url.split("/").pop() || "file"
+    document.body.appendChild(a)
+    a.click()
+
+
+
+    a.remove()
+    window.URL.revokeObjectURL(downloadUrl)
+  } catch (error) { 
+    console.log("Download failed",error)
+  }
+} 
