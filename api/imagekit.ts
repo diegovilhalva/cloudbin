9b2b0874700bce82f50571dd3ceea3fde9bc4831
Diego Vilhalva <diegorocha450@yahoo.com>
2026-02-26 10:51:49 -0300

    fixed response error

--- api/imagekit.ts
+++ api/imagekit.ts
@@ -39,7 +39,7 @@ export default async function handler(
         }
       )
 
-      return res.status(200).json(response.data)
+      return res.status(200).json({ ok: true, data: response.data })
     }
 
     // RENAME FILE
