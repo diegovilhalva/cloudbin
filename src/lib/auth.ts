48e055354f2199724d4fa9993f2d7dedca61e78c
Diego Vilhalva <89986274+diegovilhalva@users.noreply.github.com>
2026-08-12 10:55:33 -0300

    Refactor handleOAuthLogin to use new parameters

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
 import { account, OAuthProvider } from "./appwrite";
 
+
 export const handleOAuthLogin = () => {
-    account.createOAuth2Session({
-        provider:OAuthProvider.Google,
-        success:`${import.meta.env.VITE_BASE_URL}/drive/home`
-    })
-}
+    account.createOAuth2Session(
+        OAuthProvider.Google, // 1º parâmetro: Provedor
+        `${import.meta.env.VITE_BASE_URL}/drive/home`, 
+        `${import.meta.env.VITE_BASE_URL}/login`       
+    );
+};
